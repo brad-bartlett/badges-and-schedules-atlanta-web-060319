4beb3a1d3c135f2d4b0083b2f636eebbfb7343e2
Bradley Bartlett <bbartlett222@gmail.com>
2019-05-20 20:39:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_makes(name)
+  return "Hello, my name is #{name}"
+end
+
+def batch_badge_creator(array)
+  newarray = []
+  array.each do |name|
+    newarray.push("Hello, my name is #{name}")
+  end
+  return newarray
+end
+
+def assign_rooms(array)
+  newarray = []
+  counter = 1
+  array.each do |name|
+    newarray.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
+  end
+  return newarray
+end
